b57184f10ede9edec73508d38c3ee3ffb5476a7d
hiasa07 <181676759+hiasa07@users.noreply.github.com>
2025-01-19 07:48:19 +0900

    下書き機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_16_212144) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_18_221509) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_16_212144) do
     t.string "title"
     t.string "image_id"
     t.integer "user_id"
+    t.integer "status", default: 0, null: false
   end
 
   create_table "relationships", force: :cascade do |t|
