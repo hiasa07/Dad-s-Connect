b4a0d7b9aebf74ab01050b575ee922dc0fb1d40d
hiasa07 <181676759+hiasa07@users.noreply.github.com>
2025-01-25 14:29:30 +0900

    文字数制限変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 15 }
+  validates :title, presence: true, length: { maximum: 30 }
   validates :text, presence: true, length: { maximum: 195 }
   validates :image, presence: true
 
