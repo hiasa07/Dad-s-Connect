d20ed4515ab8bdda3784264f75cb582eaf4af167
hiasa07 <181676759+hiasa07@users.noreply.github.com>
2025-03-30 08:20:39 +0900

    カテゴリー機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_08_230039) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_29_225033) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -54,6 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_08_230039) do
     t.integer "user_id"
     t.integer "status", default: 0, null: false
     t.integer "views"
+    t.integer "category_id"
   end
 
   create_table "relationships", force: :cascade do |t|
